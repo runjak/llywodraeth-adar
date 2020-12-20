55066894493bbd77891273b729745d6bc23f418b
Jakob Runge <github@runjak.codes>
2020-12-20 16:47:08 +0100

    experiment with ffmpeg

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,23 @@
-console.log('Hello Birbs!', process.env['RTMP_URL']);
+import { start as startFfmpeg } from './ffmpeg';
+
+const test = () => {
+  const { kill } = startFfmpeg({
+    onStdInErr: (error) => {
+      console.log('Got error on stdIn:', error);
+      kill();
+    },
+    onStdErrErr: (data) => {
+      console.log('Got output on stdErr:', String(data));
+      kill();
+    },
+    onClose: (code, signal) => {
+      console.log('ffmpeg terminated:', { code, signal });
+    },
+  });
+
+  process.on('SIGINT', () => {
+    console.log('noticed SIGINT!', kill());
+  });
+};
+
+test();
